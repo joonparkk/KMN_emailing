732d91f623e423db2b03162756dd5a53f0dd0ca2
joonparkk <ryanjparkk02@gmail.com>
2025-04-16 21:33:39 -0700

    Implemented the bold function

--- KMN_emailing.py
+++ KMN_emailing.py
@@ -8,6 +8,7 @@ from email.mime.base import MIMEBase
 from email import encoders
 from tkinter import *
 from tkinter import filedialog
+import tkinter.font as tkFont
 
 #Automatically import the pip pandas library across different devices
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
@@ -23,7 +24,7 @@ attachment_path = ""
 body = ""
 
 #read the csv file of all emails iterating through each row
-csv_file = ""
+csv_file = "KMN_emails.csv"
 information = reader.read_csv(csv_file)
 
 def compile_list(email_list):
@@ -74,7 +75,21 @@ def get_text():
     subject = subjecttxt.get("1.0", 'end-1c')
     compile_list(information)
 
+def toggle_bold():
+    try:
+        start = inputtxt.index("sel.first")
+        end = inputtxt.index("sel.last")
 
+        # Check if the selected text has the "bold" tag
+        if "bold" in inputtxt.tag_names("sel.first"):
+            inputtxt.tag_remove("bold", start, end)
+        else:
+            inputtxt.tag_add("bold", start, end)
+    except TclError:
+        pass  # No text is selected
+
+
+    
 myWindow = Tk()
 myWindow.geometry("700x700")
 bodyLabel = Label(myWindow, text="Enter Body:")
@@ -84,9 +99,14 @@ progressLabel = Label(myWindow, text = "Progress: Emails not sent yet")
 
 subjecttxt = Text(myWindow, height = 2, width = 50, bg = "light blue")
 inputtxt = Text(myWindow, height = 10, width = 50, bg = "light yellow")
+bold_font = tkFont.Font(family="Helvetica", size=10, weight="bold")
+inputtxt.tag_configure("bold", font=bold_font)
+
 quitButton = Button(myWindow, text="Quit", command=myWindow.destroy, height= 1, width=10)
 sendButton = Button(myWindow, text = "Send", command= lambda:get_text(), height=1, width= 10)
 attachmentButton = Button(myWindow, text = "Add Attachment", command = lambda:open_file(), height=1, width=15)
+
+boldButton = Button(myWindow, text="Bold", command=toggle_bold, height=1, width=10)
 
 
 subjectLabel.pack()
@@ -98,7 +118,9 @@ inputtxt.pack()
 attachmentLabel.pack(pady=(60,0))
 attachmentButton.pack()
 
-sendButton.pack(pady=(60,0))
+boldButton.pack(pady=(30,0))
+
+sendButton.pack(pady=(30,0))
 progressLabel.pack()
 
 quitButton.pack(pady=(60,0))
